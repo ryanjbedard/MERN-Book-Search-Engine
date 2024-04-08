3ece9598803f4668cd9d20b4b4e1ca00f14c464a
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2024-04-07 20:41:55 -0700

    Update connection.js to connect to Mongo Atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://ryanjbedard:5BUvDWbyN&qE@cluster0.jjaessc.mongodb.net/Users?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
